33702e69249482ab6dfa3bca5bdc04d4e5c2cfbb
IceCream <www@withdata.net>
2023-09-20 03:21:31 +0800

    修正 JPAService

--- jdbc/jdbc-jpa/src/main/kotlin/com/IceCreamQAQ/SmartAccess/jpa/JPAService.kt
+++ jdbc/jdbc-jpa/src/main/kotlin/com/IceCreamQAQ/SmartAccess/jpa/JPAService.kt
@@ -9,7 +9,9 @@ import com.IceCreamQAQ.Yu.annotation.Config
 import com.IceCreamQAQ.Yu.hasAnnotation
 import com.IceCreamQAQ.Yu.util.dataNode.ObjectNode
 import jakarta.persistence.Entity
+import jakarta.persistence.EntityManagerFactory
 import jakarta.persistence.Table
+import java.io.Closeable
 import javax.inject.Named
 import javax.sql.DataSource
 
@@ -17,6 +19,23 @@ import javax.sql.DataSource
 abstract class JPAService(@Config db: ObjectNode) : DBService {
 
     override fun isModel(clazz: Class<*>): Boolean = clazz.hasAnnotation<Entity>() || clazz.hasAnnotation<Table>()
+
+    val databaseMap = HashMap<String, ObjectNode>()
+    val dataSourceMap = HashMap<String, DataSource>()
+    val closeFunMap = HashMap<String, EntityManagerFactory>()
+
+    override fun initDatabase(name: String, config: ObjectNode) {
+        databaseMap[name] = config
+        dataSourceMap[name] = JDBCPool.supportPool.createDataSource(name, config)
+    }
+
+    override fun closeDatabase(name: String) {
+        closeFunMap[name]?.close()
+    }
+
+    override fun close() {
+        closeFunMap.values.forEach { it.close() }
+    }
 
     override fun createAccess(
         accessClass: Class<out Access<*, *>>,
