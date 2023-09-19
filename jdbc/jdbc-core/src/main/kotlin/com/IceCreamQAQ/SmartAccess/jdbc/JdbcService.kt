f33f160c533d49a57071154a570e39b337a03a52
IceCream <www@withdata.net>
2023-09-20 00:48:12 +0800

    修正 JDBCService

--- jdbc/jdbc-core/src/main/kotlin/com/IceCreamQAQ/SmartAccess/jdbc/JdbcService.kt
+++ jdbc/jdbc-core/src/main/kotlin/com/IceCreamQAQ/SmartAccess/jdbc/JdbcService.kt
@@ -2,10 +2,14 @@ package com.IceCreamQAQ.SmartAccess.jdbc
 
 import com.IceCreamQAQ.SmartAccess.DBService
 import com.IceCreamQAQ.SmartAccess.access.Access
+import com.IceCreamQAQ.SmartAccess.access.AccessMetadataProvider
 import com.IceCreamQAQ.SmartAccess.db.DataSourceUtil
 import com.IceCreamQAQ.SmartAccess.jdbc.pool.JDBCPool
 import com.IceCreamQAQ.Yu.annotation.Config
+import com.IceCreamQAQ.Yu.hasAnnotation
 import com.IceCreamQAQ.Yu.util.dataNode.ObjectNode
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
 import javax.inject.Named
 import javax.sql.DataSource
 
@@ -23,17 +27,31 @@ class JdbcService(@Config db: ObjectNode) : DBService {
             "poolIdle"
         )
     )
-    lateinit var connectMap: Map<String, DataSource>
+    var connectMap = HashMap<String, DataSource>()
 
-    override fun start() {
-        connectMap = JDBCPool.createConnectMap(dbMap)
+    override fun initDatabase(name: String, config: ObjectNode) {
+        connectMap[name] = JDBCPool.supportPool.createDataSource(name, config)
     }
 
-    override fun makeAccess(accessInterface: Class<out Access<*, *>>) {
+    override fun startDatabase(name: String, models: List<Class<*>>) {
+
+    }
+
+    override fun closeDatabase(name: String) {
+        connectMap[name]?.connection?.close()
+    }
+
+    override fun isModel(clazz: Class<*>): Boolean = clazz.hasAnnotation<Entity>() || clazz.hasAnnotation<Table>()
+
+    override fun createAccess(
+        accessClass: Class<out Access<*, *>>,
+        modelClass: Class<*>,
+        metadataProvider: AccessMetadataProvider
+    ): Access<*, *> {
         TODO("Not yet implemented")
     }
 
     override fun close() {
-        if (::connectMap.isInitialized) connectMap.values.forEach { it.connection.close() }
+        connectMap.values.forEach { it.connection.close() }
     }
 }
--- jdbc/jdbc-core/src/main/kotlin/com/IceCreamQAQ/SmartAccess/jdbc/pool/JDBCPool.kt
+++ jdbc/jdbc-core/src/main/kotlin/com/IceCreamQAQ/SmartAccess/jdbc/pool/JDBCPool.kt
@@ -1,25 +1,20 @@
 package com.IceCreamQAQ.SmartAccess.jdbc.pool
 
-import com.IceCreamQAQ.Yu.mapMap
-import com.IceCreamQAQ.Yu.util.dataNode.ObjectNode
-import javax.sql.DataSource
-
 object JDBCPool {
 
-    val supportPools = mutableMapOf(
+    private val supportPools = mutableMapOf(
         "HikariCP" to ("com.zaxxer.hikari.HikariDataSource" to HikariPool)
     )
 
-    fun createConnectMap(dbMap: ObjectNode): Map<String, DataSource> {
-        supportPools.values.forEach { (clazz, creator) ->
-            runCatching {
-                Class.forName(clazz)
-                return dbMap.mapMap { (name, db) ->
-                    name to creator.createDataSource(
-                        name,
-                        db as? ObjectNode ?: error("数据库 $name 配置错误，数据库信息转化失败！")
-                    )
-                }
+    lateinit var supportPool: SupportPool
+        private set
+
+    init {
+        for (it in supportPools.values) {
+            try {
+                Class.forName(it.first)
+                supportPool = it.second
+            } catch (_: Exception) {
             }
         }
         error("没有引入任何受支持的连接池类型！请引入以下连接池之一：${supportPools.keys.joinToString(", ")}")
