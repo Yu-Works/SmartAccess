a88aa5738608001d621b32729b44a35eb6a1c45e
icecream <www@withdata.net>
2024-03-16 15:08:48 +0800

    dev

--- framework/hibernate/hibernate5/build.gradle.kts
+++ framework/hibernate/hibernate5/build.gradle.kts
@@ -6,4 +6,6 @@ dependencies{
     api(project(":hibernate"))
     api("org.hibernate:hibernate-core-jakarta:5.6.15.Final")
     testImplementation("com.h2database:h2:2.2.224")
+    testImplementation("com.IceCreamQAQ.Rain:test-base:1.0.0-DEV1")
+    testImplementation("com.IceCreamQAQ.Rain:event:1.0.0-DEV1")
 }
--- framework/hibernate/hibernate5/src/test/kotlin/com/IceCreamQAQ/SmartAccess/hibernate/test/main.kt
+++ framework/hibernate/hibernate5/src/test/kotlin/com/IceCreamQAQ/SmartAccess/hibernate/test/main.kt
@@ -1,1 +1,29 @@
 package com.IceCreamQAQ.SmartAccess.hibernate.test
+
+import com.IceCreamQAQ.SmartAccess.hibernate.test.model.StudentAccess
+import rain.application.FullStackApplicationLauncher
+import rain.application.events.AppStatusEvent
+import rain.event.annotation.EventListener
+import rain.event.annotation.SubscribeEvent
+import smartaccess.annotation.Transactional
+import javax.inject.Inject
+
+@EventListener
+class TestListener {
+
+    @Inject
+    lateinit var studentAccess : StudentAccess
+
+    @SubscribeEvent
+    @Transactional
+    fun AppStatusEvent.AppStarted.onEvent() {
+//        studentAccess.save(Student(name = "2", age = 1))
+//        studentAccess.print()
+        println(studentAccess.countByAge(1))
+    }
+
+}
+
+fun main(){
+    FullStackApplicationLauncher.launch()
+}
