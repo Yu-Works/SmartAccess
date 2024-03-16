17dab4abc73cf370297f24f3f28d43130b5742d3
icecream <www@withdata.net>
2024-03-16 15:08:55 +0800

    dev1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.IceCreamQAQ.SmartAccess"
-version = "0.1.0"
+version = "0.1.0-DEV1"
 
 repositories {
     mavenLocal()
@@ -15,6 +15,8 @@ repositories {
 subprojects {
     apply {
         plugin("java")
+        plugin("java-library")
+        plugin("maven-publish")
     }
 
     repositories {
@@ -26,5 +28,53 @@ subprojects {
     dependencies {
         implementation(kotlin("stdlib"))
         implementation("com.IceCreamQAQ.Rain:application:1.0.0-DEV1")
+    }
+
+    configure<PublishingExtension> {
+        publications {
+            create<MavenPublication>(name) {
+                groupId = "com.IceCreamQAQ.SmartAccess"
+                artifactId = name
+                version = rootProject.version.toString()
+
+                pom {
+                    name.set("SmartAccess Java ORM Framework")
+                    description.set("SmartAccess Java ORM Framework")
+                    url.set("https://github.com/Yu-Works/SmartAccess")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("IceCream")
+                            name.set("IceCream")
+                            email.set("www@withdata.net")
+                        }
+                    }
+                    scm {
+                        connection.set("")
+                    }
+                }
+                from(components["java"])
+            }
+
+            repositories {
+                mavenLocal()
+                maven {
+                    val snapshotsRepoUrl = "https://maven.icecreamqaq.com/repository/maven-snapshots/"
+                    val releasesRepoUrl = "https://maven.icecreamqaq.com/repository/maven-releases/"
+                    url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+
+
+                    credentials {
+                        username = ""
+                        password = ""
+                    }
+                }
+            }
+        }
     }
 }
